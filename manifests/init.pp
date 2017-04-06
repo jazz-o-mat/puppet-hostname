9890d7c8af7296d09fc625904d40f5511aee4710
Yves Zieger <jazz-o-mat@users.noreply.github.com>
2017-04-06 22:20:24 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,36 +17,18 @@ class hostname (
 ) {
 
   exec { 'set computername':
-    command => "sudo /usr/sbin/scutil --set ComputerName ${hostname}",
-    unless  => "test `scutil --get ComputerName` == '${hostname}'",
-    require => Sudoers::Allowed_Command['hostname_scutil']
+    command => "/usr/sbin/scutil --set ComputerName ${hostname}",
+    unless  => "test `scutil --get ComputerName` == '${hostname}'"
   }
 
   exec { 'set hostname':
-    command => "sudo /usr/sbin/scutil --set HostName ${hostname}",
-    unless  => "test `scutil --get HostName` == '${hostname}'",
-    require => Sudoers::Allowed_Command['hostname_scutil']
+    command => "/usr/sbin/scutil --set HostName ${hostname}",
+    unless  => "test `scutil --get HostName` == '${hostname}'"
   }
 
   exec { 'set localhostname':
-    command => "sudo /usr/sbin/scutil --set LocalHostName ${hostname}",
-    unless  => "test `scutil --get LocalHostName` == '${hostname}'",
-    require => Sudoers::Allowed_Command['hostname_scutil']
+    command => "/usr/sbin/scutil --set LocalHostName ${hostname}",
+    unless  => "test `scutil --get LocalHostName` == '${hostname}'"
   }
 
-  boxen::osx_defaults { 'set netbiosname':
-    user   => $::boxen_user,
-    key    => 'NetBIOSName',
-    domain => 'com.apple.smb.server',
-    type   => 'string',
-    value  => $hostname
-  }
-
-  sudoers::allowed_command{ 'hostname_scutil':
-    command          => '/usr/sbin/scutil',
-    user             => $::boxen_user,
-    require_password => false,
-    comment          => 'Allow boxen user to control scutil',
-    require_exist    => false
-  }
 }
